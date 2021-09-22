56877229f78ad70e41cf7cfd595c799cbe02c91c
cosmo <cosmo8082@163.com>
2021-09-22 15:02:14 +0800

    2021-9-22 edit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RL-snakegame
+
+## 基于强化学习和Pytorch的贪吃蛇小游戏
---
+++ helper.py
@@ -0,0 +1,20 @@
+from os import wait
+import matplotlib.pyplot as plt
+from IPython import display
+
+
+
+def plot(scores, mean_scores):
+    
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.cla()
+    plt.title('Training')
+    plt.xlabel('Number of Games')
+    plt.ylabel('Score')
+    plt.plot(scores)
+    plt.plot(mean_scores)
+    plt.ylim(ymin=0)
+    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.pause(0.1)
