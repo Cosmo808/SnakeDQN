eebfe5849cc6ef171ba77819e628e204199f08bb
Cosmo808 <cosmo8082@163.com>
2022-05-05 20:09:48 +0800

    2022-5-5 edit

--- torch_model.py
+++ torch_model.py
@@ -55,7 +55,7 @@ class QTrainer:
             Q_new = reward[idx]
             if not done[idx]:
                 Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
-            target[idx][torch.argmax(action).item()] = Q_new
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
 
         self.optimizer.zero_grad()
         loss = self.criterion(target, predict)
